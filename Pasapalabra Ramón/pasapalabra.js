a1a30628a1b9f83ee5d8138494fdb9e2f3478263
Ramon941 <101798258+Ramon941@users.noreply.github.com>
2022-12-20 15:24:57 +0100

    Add files via upload

---
+++ Pasapalabra Ramón/pasapalabra.js
@@ -0,0 +1,493 @@
+//Preguntas
+var science = [
+	{
+		"Question": "<b>Empieza por A:</b> Dispositivo que convierte una señal eléctrica de audio en ondas mecánicas de sonido",
+		"Respuesta": "altavoz"
+	},
+	{
+		"Question": "<b>Empieza por B:</b> Caja donde se suele guardar los medicamentos",
+		"Respuesta": "botiquin"
+	},
+	{
+		"Question": "<b>Empieza por C:</b> Jefe de la mafia, especialmente de narcotraficantes",
+		"Respuesta": "capo"
+	},
+	{
+		"Question": "<b>Empieza por D:</b> Animal mitológico con figura de serpiente corpulenta, garras y alas que echa fuego por la boca",
+		"Respuesta": "dragon"
+	},
+	{
+		"Question": "<b>Empieza por E:</b> Propiedad de un cuerpo sólido para recuperar su forma cuando cesa la fuerza que la altera",
+		"Respuesta": "elasticidad"
+	},
+	{
+		"Question": "<b>Empieza por F:</b> Prenda interior elástica que ciñe la cintura y las caderas",
+		"Respuesta": "faja"
+	},
+	{
+		"Question": "<b>Empieza por G:</b> Lance del juego en que un equipo consigue introducir la pelota en la portería contraria",
+		"Respuesta": "gol"
+	},
+	{
+		"Question": "<b>Empieza por H:</b> Pieza de metal que sirve para adornar o cerrar bolsos, cinturones, etc...",
+		"Respuesta": "hebilla"
+	},
+	{
+		"Question": "<b>Empieza por I:</b> País cuya capital es Reikiavik",
+		"Respuesta": "islandia"
+	},
+	{
+		"Question": "<b>Empieza por J:</b> Objeto ornamental usado para adornar o embellecer el cuerpo",
+		"Respuesta": "joya"
+	},
+	{
+		"Question": "<b>Empieza por K:</b> Persona que realiza una acción temeraria con propósito suicida o con riesgo de su vida",
+		"Respuesta": "kamikaze"
+	},
+	{
+		"Question": "<b>Empieza por L:</b> Gran depósito natural de agua en una depresión del terreno",
+		"Respuesta": "lago"
+	},
+	{
+		"Question": "<b>Empieza por M:</b> Conserva de fruta cocida en azucar",
+		"Respuesta": "mermelada"
+	},
+	{
+		"Question": "<b>Empieza por N:</b> Arma cuya hoja pivota sobre un eje que la une al mango o cabo, para que el filo quede guardado",
+		"Respuesta": "navaja"
+	},
+	{
+		"Question": "<b>Empieza por O:</b> Llegar a tener cierta cosa que se quiere, se solicita o se merece",
+		"Respuesta": "obtener"
+	},
+	{
+		"Question": "<b>Empieza por P:</b> Espacio largo y estrecho de un edificio o una casa que comunica unas estancias con otras",
+		"Respuesta": "pasillo"
+	},
+	{
+		"Question": "<b>Empieza por Q:</b> Capital de Ecuador",
+		"Respuesta": "quito"
+	},
+	{
+		"Question": "<b>Empieza por R:</b> Indicativo del presente la tercera persona del singular del verbo redimir",
+		"Respuesta": "redime"
+	},
+	{
+		"Question": "<b>Empieza por S:</b> Apellido del escritor de los libros El gato con sombrero y El Lorax",
+		"Respuesta": "seuss"
+	},
+	{
+		"Question": "<b>Empieza por T:</b> Adorno semicircular que cubre solo una zona de la cabeza",
+		"Respuesta": "tiara"
+	},
+	{
+		"Question": "<b>Empieza por U:</b> Derecho real que permite el disfrute de una cosa ajena de la que no se tiene la propiedad",
+		"Respuesta": "usufructo"
+	},
+	{
+		"Question": "<b>Empieza por V:</b> Nombre del principe de Valaquia, conocido como 'El empalador'",
+		"Respuesta": "vlad"
+	},
+	{
+		"Question": "<b>Empieza por W</b> Persona que practica waterpolo",
+		"Respuesta": "waterpolista"
+	},
+	{
+		"Question": "<b>Contiene la letra X:</b> Sacar a una persona o cosa del lugar en el que estaba enterrada; especialmente un cadáver.",
+		"Respuesta": "exhumar"
+	},
+	{
+		"Question": "<b>Contiene la letra Y:</b> Preparar el montaje y ejecución de un espectáculo antes de ofrecerlo al público",
+		"Respuesta": "ensayar"
+	},
+	{
+		"Question": "<b>Empieza por Z:</b> Hombre que demuestra cariño de una forma exagerada, generalmente para conseguir algo.",
+		"Respuesta": "zalamero"
+	}
+];
+
+var science2 = [
+	{
+		"Question": "<b>Empieza por A:</b> Acción por la cual, tras un proceso, un filo es mas cortante",
+		"Respuesta": "afilar"
+	},
+	{
+		"Question": "<b>Empieza por B:</b> Dispositivo que emite luz debido al paso de corriente eléctrica a través de un filamento que se encuentra en el interior de un cuerpo",
+		"Respuesta": "bombilla"
+	},
+	{
+		"Question": "<b>Empieza por C:</b> Traducción del ingles 'vaquero'",
+		"Respuesta": "cowboy"
+	},
+	{
+		"Question": "<b>Empieza por D:</b> Objeto preparado para mostrar un resultado aleatorio cuando es lanzado",
+		"Respuesta": "dado"
+	},
+	{
+		"Question": "<b>Empieza por E:</b> Que dura poco tiempo o es pasajero",
+		"Respuesta": "efimero"
+	},
+	{
+		"Question": "<b>Empieza por F:</b> Engaño o mentira que se esconde bajo algo, en especial cuando se pone de manifiesto su falta de verdad",
+		"Respuesta": "falacia"
+	},
+	{
+		"Question": "<b>Empieza por G:</b> Hueso de fruta, especialmente el del albaricoque, con que juegan los niños",
+		"Respuesta": "guito"
+	},
+	{
+		"Question": "<b>Empieza por H:</b> Representación de imágenes tridimensionales basada en el empleo de la luz",
+		"Respuesta": "holograma"
+	},
+	{
+		"Question": "<b>Empieza por I:</b> Isla pequeña y generalmente despoblada, en especial cuando es de origen volcánico",
+		"Respuesta": "islote"
+	},
+	{
+		"Question": "<b>Empieza por J:</b> Región corporal que presentan algunas especies de mamíferos artiodáctilos de zonas desértico-cálidas",
+		"Respuesta": "joroba"
+	},
+	{
+		"Question": "<b>Empieza por K:</b> Variedad de yōkai o demonio perteneciente al folclore japonés que habita en los ríos o lagos",
+		"Respuesta": "kappa"
+	},
+	{
+		"Question": "<b>Empieza por L:</b> Área de transición entre los sistemas terrestres y los marinos",
+		"Respuesta": "litoral"
+	},
+	{
+		"Question": "<b>Empieza por M:</b> Que puede ser moldeado o trabajado con facilidad",
+		"Respuesta": "maleable"
+	},
+	{
+		"Question": "<b>Empieza por N:</b> Comunidad autónoma cuya capital es Pamplona",
+		"Respuesta": "navarra"
+	},
+	{
+		"Question": "<b>Empieza por O:</b> Dicho de una persona o ente que todo lo puede",
+		"Respuesta": "omnipotente"
+	},
+	{
+		"Question": "<b>Empieza por P:</b> Nombre del escritor 'Veinte Poemas de amor'",
+		"Respuesta": "pablo"
+	},
+	{
+		"Question": "<b>Empieza por Q:</b> Juego de apuestas en el cual los participantes rellenan un boleto con los pronósticos de los resultados de cierta competición deportiva",
+		"Respuesta": "quiniela"
+	},
+	{
+		"Question": "<b>Empieza por R:</b> Que es despreciable por cometer o ser capaz de cometer malas acciones, con falsedad, hipocresía, traición o engaño",
+		"Respuesta": "ruin"
+	},
+	{
+		"Question": "<b>Empieza por S:</b> Que es puramente aquello que se dice, sin ninguna característica especial o singular",
+		"Respuesta": "simple"
+	},
+	{
+		"Question": "<b>Empieza por T:</b> Recipiente de barro con forma de vasija de perfil ovalado, boca y pie estrechos y por lo general sin asas",
+		"Respuesta": "tinaja"
+	},
+	{
+		"Question": "<b>Empieza por U:</b> Derecho real que permite el disfrute de una cosa ajena de la que no se tiene la propiedad",
+		"Respuesta": "usufructo"
+	},
+	{
+		"Question": "<b>Empieza por V:</b> Líquido con sabor agrio, que proviene de la fermentación acética del alcohol, como la de vino y manzana",
+		"Respuesta": "vinagre"
+	},
+	{
+		"Question": "<b>Empieza por W</b> Dispositivo portátil que, por medio de una bobina electromagnética, permite sólamente la lectura del contenido del casette de audio",
+		"Respuesta": "walkman"
+	},
+	{
+		"Question": "<b>Contiene la letra X:</b> Que se considera adecuado o que está pensado para hombres y mujeres",
+		"Respuesta": "unisex"
+	},
+	{
+		"Question": "<b>Contiene la letra Y:</b> Pais cuya capital es Montevideo",
+		"Respuesta": "uruguay"
+	},
+	{
+		"Question": "<b>Empieza por Z:</b> Resolver de un modo definitivo un asunto que presenta dificultades o inconvenientes",
+		"Respuesta": "zanjar"
+	}
+];
+
+var drea = [
+	{
+		"Question": "<b>Empieza por A:</b> Que resulta difícil de entender por tener el carácter esquemático y poco concreto propio (femeninino)",
+		"Respuesta": "abstracta"
+	},
+	{
+		"Question": "<b>Empieza por B:</b> Cada uno de los dos conductos fibrocartilaginosos en que se bifurca la tráquea y que entran en los pulmones.",
+		"Respuesta": "bronquio"
+	},
+	{
+		"Question": "<b>Empieza por C:</b> Pelea entre personas o animales.",
+		"Respuesta": "combate"
+	},
+	{
+		"Question": "<b>Empieza por D:</b> Un instrumento de viento tradicional de los pueblos aborígenes australianos.",
+		"Respuesta": "didyeridu"
+	},
+	{
+		"Question": "<b>Empieza por E:</b> Expresión coloquial dicho cuando se halla o descubre algo que se busca con afán.",
+		"Respuesta": "eureka"
+	},
+	{
+		"Question": "<b>Empieza por F:</b> Figura irreal, imaginaria o fantástica y normalmente incorpórea.",
+		"Respuesta": "fantasma"
+	},
+	{
+		"Question": "<b>Empieza por G:</b> Legumbre de la especie 'Pisum sativum' formada por una vaina y semillas con formas esféricas.",
+		"Respuesta": "guisante"
+	},
+	{
+		"Question": "<b>Empieza por H:</b> Muy frío (de temperatura inferior a la ordinaria).",
+		"Respuesta": "helado"
+	},
+	{
+		"Question": "<b>Empieza por I:</b> Dicese de la persona con falta de fuerza, poder o competencia para realizar una cosa, hacer que suceda o ponerle resistencia..",
+		"Respuesta": "impotente"
+	},
+	{
+		"Question": "<b>Empieza por J:</b> Una matriz formada por las derivadas parciales de primer orden de una función. Una de las aplicaciones más interesantes de esta matriz es la posibilidad de aproximar linealmente a la función en un punto.",
+		"Respuesta": "jacobiano"
+	},
+	{
+		"Question": "<b>Empieza por K:</b> Un plato coreano hecho a base de preparación fermentada que tiene como ingrediente básico la col asiática o repollo.",
+		"Respuesta": "kimchi"
+	},
+	{
+		"Question": "<b>Empieza por L:</b> El séptimo signo del zodíaco, el cuarto de naturaleza positiva y el tercero de cualidad cardinal.",
+		"Respuesta": "libra"
+	},
+	{
+		"Question": "<b>Empieza por M:</b> Personaje cuya características más notables son su piel pálida y largas trenzas oscuras. Rara vez muestra emoción y generalmente es amarga.",
+		"Respuesta": "miercoles"
+	},
+	{
+		"Question": "<b>Empieza por N:</b> Célula nerviosa, que generalmente consta de un cuerpo de forma variable y provisto de diversas prolongaciones.",
+		"Respuesta": "neurona"
+	},
+	{
+		"Question": "<b>Empieza por O:</b> El unico mamífero que pone huevos.",
+		"Respuesta": "ornitorrinco"
+	},
+	{
+		"Question": "<b>Empieza por P:</b> Unidad lingüística, dotada generalmente de un significado, que se separa de las demás mediante pausas en la pronunciación y espacios en blanco en la escritura.",
+		"Respuesta": "palabra"
+	},
+	{
+		"Question": "<b>Empieza por Q:</b> Sirve para aclarar que aquello que se expresa puede tanto ser cierto como no.",
+		"Respuesta": "quizas"
+	},
+	{
+		"Question": "<b>Empieza por R:</b> Brillo de algunas cosas.",
+		"Respuesta": "resplandor"
+	},
+	{
+		"Question": "<b>Empieza por S:</b> Extraordinario, raro o excelente.",
+		"Respuesta": "singular"
+	},
+	{
+		"Question": "<b>Empieza por T:</b> Alteración leve de la salud.",
+		"Respuesta": "trastorno"
+	},
+	{
+		"Question": "<b>Empieza por U:</b> Primero (que precede a los demás de su especie cardinal).",
+		"Respuesta": "uno"
+	},
+	{
+		"Question": "<b>Empieza por V:</b> Appelido del escritor estadounidense, cuyas obras como 'Las sirenas de Titán' pertenecen al género de la ciencia ficción. ",
+		"Respuesta": "vonnegut"
+	},
+	{
+		"Question": "<b>Empieza por W</b> También conocida como La capital del aire, es la mayor ciudad del estado estadounidense de Kansas",
+		"Respuesta": "wichita"
+	},
+	{
+		"Question": "<b>Contiene la letra X:</b> Resultado feliz de un negocio, actuación, etc.",
+		"Respuesta": "exito"
+	},
+	{
+		"Question": "<b>Contiene la letra Y:</b> Línea de luz que procede de un cuerpo luminoso, y especialmente las que vienen del sol.",
+		"Respuesta": "rayo"
+	},
+	{
+		"Question": "<b>Empieza por Z:</b> Meterse debajo del agua con ímpetu.",
+		"Respuesta": "zambullirse"
+	}
+];
+
+//Funciones
+
+var solucion;
+var letra = document.getElementsByClassName("item");
+var score = document.getElementById("puntuacion");
+var contador = 0;
+var puntuacion = 0;
+var contadorgreen = 0;
+var contadorrojo = 0;
+elegirPregunta(contador);//La primera pregunta siempre
+
+
+function select_id(id) {
+	return document.getElementById(id)
+}
+
+function elegirPregunta(n) {
+	if (puntuacion < 26) {
+		do {
+			if (letra[n].style.backgroundColor == "green" || letra[n].style.backgroundColor == "red") {
+				do {
+					n++;//Contador hasta que encuentre la posición sin responder del rosco
+					if (n >= 26) {
+						n = 0;
+					}
+				} while (letra[n].style.backgroundColor == "green" || letra[n].style.backgroundColor == "red")
+			}
+			funciona = drea[n];//Iguala a la posición que asignes del array
+			select_id("pregunta").innerHTML = funciona.Question;
+			letra[n].style.backgroundColor = "orange";
+			/*if (select_id("pregunta").innerHTML == "<b>Song Empieza por  W</b>") {
+				var sonido2 = document.createElement("iframe");
+				sonido2.setAttribute("src", "canciontop.mp3");
+				sonido2.setAttribute('hidden', true);
+				document.body.appendChild(sonido2);
+			}*/
+			solucion = funciona.Respuesta;//Saco el valor para luego hacer la comparación
+			contador = n;//Asi la pregunta va siempre coordinada con la posición actual en el rosco
+		} while (letra[n].style.backgroundColor == "green" || letra[n].style.backgroundColor == "red");
+	}
+}
+
+//Comprueba y valida si la respuesta es correcta
+function validar() {
+	var respuestaU = document.getElementById("respuesta").value.toLowerCase();
+	if (respuestaU != "") {
+		if (puntuacion <= 25) {
+			if (respuestaU == solucion) {
+				letra[contador].style.backgroundColor = "green";
+				score.innerText++;
+				puntuacion++;
+				if (puntuacion == 26) {
+					resultado();
+				}
+				contador++;
+				if (contador == 26) {
+					contador = 0;
+				}
+				elegirPregunta(contador);
+				var boton = document.getElementsByTagName("input")[0].value = "";
+			} else {
+				mostrar();
+				letra[contador].style.backgroundColor = "red";
+				//alert("The correct answer is: " + solucion);
+				puntuacion++;
+				if (puntuacion == 26) {
+					resultado();
+				}
+				contador++;
+				if (contador == 26) {
+					contador = 0;
+				}
+				elegirPregunta(contador);
+				var boton = document.getElementsByTagName("input")[0].value = "";
+			}
+		}
+	}
+}
+
+//Mensaje de error cuando fallas
+function mostrar() {
+	swal('La respuesta correcta era: ' + solucion.toUpperCase(), '', 'error');
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src", "error.mp3");
+	sonido.setAttribute('hidden', true);
+	document.body.appendChild(sonido);
+}
+
+//Mensaje cuando finaliza la partida
+function final() {
+	if (contadorgreen >= 20) {
+		swal("Resultado: " + contadorgreen + " aciertos / " + contadorrojo + " errores", '', 'success');
+		var sonido3 = document.createElement("iframe");
+		sonido3.setAttribute("src", "ganarD.mp3");
+		sonido3.setAttribute('hidden', true);
+		document.body.appendChild(sonido3);
+	} else {
+		swal("Resultado: " + contadorgreen + " aciertos / " + contadorrojo + " errores", '', 'error');
+		var sonido3 = document.createElement("iframe");
+		sonido3.setAttribute("src", "perderD.mp3");
+		sonido3.setAttribute('hidden', true);
+		document.body.appendChild(sonido3);
+	}
+}
+
+//Salta a la siguiente pregunta
+function pasapalabra() {
+	letra[contador].style.backgroundColor = "rgb(85, 85, 255)";
+	contador++;
+	if (contador == 26) {
+		contador = 0;
+	}
+	elegirPregunta(contador);
+	var boton = document.getElementsByTagName("input")[0].value = "";
+}
+
+//Resultado de la partida
+function resultado() {
+	for (let i = 0; i < science.length; i++) {
+		if (letra[i].style.backgroundColor == "green") {
+			contadorgreen++;
+		} else {
+			contadorrojo++;
+		}
+	}
+	var boton = document.getElementsByTagName("input")[0].disabled = true;
+	var boton = document.getElementsByTagName("input")[1].disabled = true;
+	var boton = document.getElementsByTagName("input")[2].disabled = true;
+	final();
+}
+
+//Tecla
+document.addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
+		validar();
+	}
+});
+
+document.addEventListener("keyup", function (event) {
+	if (event.keyCode === 32) {
+		pasapalabra();
+	}
+});
+
+//Temporizador
+var timeout;
+
+function cuentaatras() {
+	seconds = document.getElementById("reloj");
+	if (seconds.innerText == 0) {
+		clearInterval(timeout);
+		seconds.innerText = 301;
+		resultado();
+	}
+	seconds.innerText--;
+}
+
+function timer() {
+	timeout = setInterval(cuentaatras, 1000);
+}
+
+/*
+function resetear() {
+	for (let i = 0; i < science.length; i++) {
+		letra[i].style.backgroundColor = "rgb(85, 85, 255)";
+		puntuacion = 0;
+		contador = 0;
+		score.innerText = 0;
+		elegirPregunta(0);
+	}
+}*/
